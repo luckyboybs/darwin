f72d10c800fb9718b1a2e74455d9dbf5a821ed20
luckyboybs <luckyboylh2012@gmail.com>
2024-12-07 15:32:46 +0000

    PRODUCT_STATIC_BOOT_CONTROL_HAL删除

--- device.mk
+++ device.mk
@@ -16,16 +16,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service \
 
 PRODUCT_PACKAGES += \
-    bootctrl.kona
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.kona \
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl.kona.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
