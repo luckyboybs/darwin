eb42693658d0a8c1d54a52f9a4ffb9570f5b0fc1
luckyboybs <luckyboylh2012@gmail.com>
2024-12-08 17:34:13 +0800

    编译问题

--- twrp_darwin.mk
+++ twrp_darwin.mk
@@ -7,7 +7,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from darwin device
