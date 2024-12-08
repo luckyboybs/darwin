b21c5794b23916d598acc52a150ccbe03fbfc6b1
luckyboybs <luckyboylh2012@gmail.com>
2024-12-08 18:49:25 +0800

    编译问题

--- twrp_darwin.mk
+++ twrp_darwin.mk
@@ -15,7 +15,6 @@ $(call inherit-product, device/deltainno/darwin/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := darwin
