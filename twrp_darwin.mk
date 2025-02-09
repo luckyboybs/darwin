ec2501bfdbb809a3e3b3ec09786e5ad59b1a93ee
luckyboybs <luckyboylh2012@gmail.com>
2025-02-09 20:55:25 +0800

    boot修复

--- twrp_darwin.mk
+++ twrp_darwin.mk
@@ -25,8 +25,8 @@ PRODUCT_MODEL := DT2002C
 PRODUCT_MANUFACTURER := deltainno
 
 # Common path for device trees
-#COMMON_PATH := device/$(PRODUCT_BRAND)/$(COMMON_SOC)-common
-COMMON_PATH := device/deltainno/$(COMMON_SOC)-common
+COMMON_PATH := device/$(PRODUCT_BRAND)/$(COMMON_SOC)-common
+#COMMON_PATH := device/deltainno/$(COMMON_SOC)-common
 
 # Theme
 TW_STATUS_ICONS_ALIGN := center
