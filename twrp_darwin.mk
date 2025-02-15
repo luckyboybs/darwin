764df8694ccb205a2f319cf6f75eccda01092e6d
luckyboybs <luckyboylh2012@gmail.com>
2025-02-15 21:08:08 +0800

    test

--- twrp_darwin.mk
+++ twrp_darwin.mk
@@ -4,6 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+DEVICE_PATH := device/deltainno/darwin
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
