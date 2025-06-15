8fc1756cd852b7624e29efada4a908bf924d89d1
luckyboybs <luckyboylh2012@gmail.com>
2025-06-15 20:38:34 +0800

    test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TARGET_NO_KERNEL := false
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm country.info=chinaxxx buildvariant=user
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm country.info=chinaxxx
 BOARD_KERNEL_IMAGE_NAME := Image  #kernel
 BOARD_KERNEL_SEPARATED_DTBO := true  #DTB
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
