0827bd8137d5f55153debb0dd327a2a6b0ee9fab
luckyboybs <luckyboylh2012@gmail.com>
2025-10-07 10:33:23 +0800

    内核调试日志,avb安全禁用

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,7 @@ TW_INCLUDE_REPACKTOOLS := true
 TARGET_NO_KERNEL := false
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm country.info=chinaxxx
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 androidboot.selinux=permissive swiotlb=2048 loop.max_part=7 firmware_class.path=/vendor/firmware_mnt/image loglevel=8 cgroup.memory=nokmem,nosocket reboot=panic_warm country.info=chinaxxx
 BOARD_KERNEL_IMAGE_NAME := Image  #kernel
 BOARD_KERNEL_SEPARATED_DTBO := true  #DTB
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
@@ -125,9 +125,19 @@ QCOM_BOARD_PLATFORMS += kona
 
 # Kenel dtb
 
-
+# 禁用 AVB (Android Verified Boot) 验证
+BOARD_AVB_ENABLE := false
+# 禁用 dm-verity (设备映射验证)
+BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+# 禁用启动验证
+BOARD_AVB_ROLLBACK_INDEX := 0
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --rollback_index 0
+# 跳过验证
+PRODUCT_SKIP_BOOT_VERITY_CHECK := true
+PRODUCT_SKIP_BOOT_VERITY := true
 # Verified Boot
-BOARD_AVB_ENABLE := true
+#BOARD_AVB_ENABLE := true
 BOARD_AVB_VBMETA_SYSTEM := system
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
 BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
